324db86686b541b7229558b88194f6a4fac4d682
Daan Mooij <daanmooij@gmail.com>
2018-02-14 20:22:44 +0100

    add v-bind:href for a tag

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 new Vue({
 	el: '#app',
 	data: {
-		title: 'Hello World!'
+		title: 'Hello World!',
+		link: 'http://google.com'
 	},
 	methods: {
 		changeTitle: function(event) {
 			this.title = event.target.value;
+		},
+		sayHello: function() {
+			return 'Hello';
 		}
 	}
 });
