6276fea3161c12789aa0cf07db36eaaa56b0d257
Daan Mooij <daanmooij@gmail.com>
2018-02-20 22:06:25 +0100

    add solution to exercise 2

--- exercise_2/app.js
+++ exercise_2/app.js
@@ -2,5 +2,13 @@ new Vue({
         el: '#exercise',
         data: {
             value: ''
+        },
+        methods: {
+        	showAlert: function() {
+        		alert('Hello');
+        	},
+        	storeValue: function(event) {
+        		this.value = event.target.value;
+        	}
         }
     });
